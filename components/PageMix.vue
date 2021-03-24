c90768c6c451a3f36e242665f73a53fcb5f52a0f
benjamin_kh <benjamin_kh@malam-payroll.com>
2021-03-24 07:20:03 +0200

    trying to make the about page and page mix template work

--- components/PageMix.vue
+++ components/PageMix.vue
@@ -1,74 +1,70 @@
 <template>
-    <div class="page-mix page-page" :id="pageId">
-        <transition name="fade">
-            <Loading v-if="loading" />
-        </transition>
-        <slot name="page-content">
-            <div class="page-title-container">
-                <div class="container">
-                    <h1 id="page-title">
-                        <span class="lang" v-html="pageTitle"></span>
-                    </h1>
-                </div>
-            </div>
-            <div class="container">
-                <div id="page-content" v-html="pageContent"></div>
-            </div>
-        </slot>
-    </div>
+  <div class="page-mix page-page" :id="pageId">
+    <transition name="fade">
+      <Loading v-if="loading"/>
+    </transition>
+    <slot name="page-content">
+      <div class="page-title-container">
+        <div class="container">
+          <h1 id="page-title">
+            <span class="lang" v-html="pageTitle"></span>
+          </h1>
+        </div>
+      </div>
+      <div class="container">
+        <div id="page-content" v-html="pageContent"></div>
+      </div>
+    </slot>
+  </div>
 </template>
 
 <script>
 import Loading from '@/components/Loading'
+
 export default {
-    name: 'PageMix',
-    components: {
-        Loading
-    },
-    metaInfo() {
-        return {
-            title: this.pageTitle,
-        }
-    },
-    data() {
-        return {
-            dataEntryId: '',
-            pageTitle: '',
-            pageContent: '',
-            loading: true
-        }
-    },
-    props: {
-        pageId: {
-            type: String
-        }
-    },
-    mounted() {
-        // this.dataEntryId = this.entryId;
-        // if (!this.dataEntryId) {
-        //     this.dataEntryId = this.$route.meta.entryId
-        // }
-        // this.getContent();
-        // this.$root.$on('langChanged', this.getContent);
-    },
-  async asyncData({app}) {
-    try {
-      const pages = await app.flamelink.content.get({
-        schemaKey: 'pages',
-        entryId: this.dataEntryId,
-        // populate: true
-      })
-        .then(pageContent => {
-          this.pageTitle = pageContent.title;
-          this.pageContent = pageContent.content;
-          this.loading = false;
-        })
-        .catch(error => console.error('Something went wrong while retrieving the entry. Details:', error));
-      return {pages}
-    } catch (err) {
-      console.log(err)
-      return {pages: []}
+  name: 'PageMix',
+  components: {
+    Loading
+  },
+  metaInfo() {
+    return {
+      title: this.pageTitle,
     }
-  }
+  },
+  data() {
+    return {
+      dataEntryId: '',
+      pageTitle: '',
+      pageContent: '',
+      loading: true
+    }
+  },
+  props: {
+    pageId: {
+      type: String
+    }
+  },
+  mounted() {
+    this.dataEntryId = this.entryId;
+    if (!this.dataEntryId) {
+      this.dataEntryId = this.$route.meta.entryId
+    }
+  },
+  async asyncData({app}) {
+    const pages = await app.flamelink.content.get({
+      schemaKey: 'pages',
+      entryId: this.dataEntryId,
+      // populate: true
+    })
+      .then(pageContent => {
+        console.log(pageContent);
+        this.pageTitle = pageContent.title;
+        this.pageContent = pageContent.content;
+        this.loading = false;
+      })
+      .catch(error => console.error('Something went wrong while retrieving the entry. Details:', error));
+    console.log(pages);
+    return {pages}
+  },
 };
 </script>
